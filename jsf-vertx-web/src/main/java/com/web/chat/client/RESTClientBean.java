5fc869987c67bcb75f62f21e77c3b0dd6af32929
ARMEN ARZUMANYAN <armen.arzumanyan.external@atos.net>
2017-06-14 15:01:04 +0400

    up to date

--- jsf-vertx-web/src/main/java/com/web/chat/client/RESTClientBean.java
+++ jsf-vertx-web/src/main/java/com/web/chat/client/RESTClientBean.java
@@ -10,13 +10,12 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.ParseException;
-import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 import org.apache.http.util.EntityUtils;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -42,9 +41,8 @@ public class RESTClientBean implements Serializable {
     }
 
     public Integer sendMessage(Message message) {
-        HttpClient httpClient = new DefaultHttpClient();
         Integer userId = 0;
-        try {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             HttpPost request = new HttpPost("http://localhost:9999/api/message");
             JSONObject json = new JSONObject();
             json.put("username", message.getUsername());
@@ -53,48 +51,29 @@ public class RESTClientBean implements Serializable {
             request.addHeader("content-type", "application/json");
             request.addHeader("charset", "utf8");
             request.setEntity(params);
-            HttpResponse response = httpClient.execute(request);
-            HttpEntity entity = response.getEntity();
-            userId = ParamUtil.integerValue(EntityUtils.toString(entity));
-        } catch (IOException ex) {
-        } finally {
-            httpClient.getConnectionManager().shutdown();
+            try (CloseableHttpResponse httpResponse = httpClient.execute(request)) {
+                ObjectMapper mapper = new ObjectMapper();
+                userId = ParamUtil.integerValue(EntityUtils.toString(httpResponse.getEntity()));
+            }
+
+        } catch (IOException e) {
+
         }
         return userId;
     }
 
-    public List<String> getMessageListddd() {
-        List<String> list = new ArrayList<>();
-        try {
-            HttpClient client = new DefaultHttpClient();
+    public List<Message> getMessageList() {
+        List<Message> list = new ArrayList<>();
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             HttpGet request = new HttpGet("http://localhost:9999/api/messagelist");
-            HttpResponse response = client.execute(request);
-            HttpEntity entity = response.getEntity();
-            ObjectMapper mapper = new ObjectMapper();
-            System.out.println("entity " + EntityUtils.toString(entity));
-            if (entity != null) {
-                //String str  = mapper.readValue(EntityUtils.toString(entity), String.class);
-                list.add(entity.toString());
+            try (CloseableHttpResponse httpResponse = httpClient.execute(request)) {
+                HttpEntity entity = httpResponse.getEntity();
+                ObjectMapper mapper = new ObjectMapper();
+                if (entity != null) {
+                    list = mapper.readValue(EntityUtils.toString(entity), List.class);
+                }
             }
         } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return list;
-    }
-
-    public List<Message> getMessageList() {
-        List<Message> list = null;
-        try {
-            HttpClient client = new DefaultHttpClient();
-            HttpGet request = new HttpGet("http://localhost:9999/api/messagelist");
-            HttpResponse response = client.execute(request);
-            HttpEntity entity = response.getEntity();
-            ObjectMapper mapper = new ObjectMapper();
-            if (entity != null) {
-                list = mapper.readValue(EntityUtils.toString(entity), List.class);
-            }
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
         }
         return list;
     }
