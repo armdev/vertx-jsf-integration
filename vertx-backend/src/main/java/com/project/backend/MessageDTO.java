8588560538a31bbe3eb76e23703efed686f56e4d
ARMEN ARZUMANYAN <armen.arzumanyan.external@atos.net>
2017-06-14 15:20:24 +0400

    up to date

--- vertx-backend/src/main/java/com/project/backend/MessageDTO.java
+++ vertx-backend/src/main/java/com/project/backend/MessageDTO.java
@@ -1,65 +1,27 @@
 package com.project.backend;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  *
- * @author armenar
+ * @author armdev
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class MessageDTO {
 
+    @Setter
+    @Getter
     private String username;
-    private String message;
-
-    public MessageDTO() {
-    }
-
-    public MessageDTO(String username, String message) {
-        this.username = username;
-        this.message = message;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 31 * hash + Objects.hashCode(this.username);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final MessageDTO other = (MessageDTO) obj;
-        return Objects.equals(this.username, other.username);
-    }
-
-    @Override
-    public String toString() {
-        return "Message{" + "username=" + username + ", message=" + message + '}';
-    }
+    @Setter
+    @Getter
+    private String message; 
 
 }
