3f70392e2812db104db2409cf6f39f73c1a8bf99
ARMEN ARZUMANYAN <armen.arzumanyan.external@atos.net>
2017-06-15 15:46:38 +0400

    added database, active jdbc

---
+++ vertx-backend/src/main/resources/db.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate v9.50 
+MySQL - 5.5.5-10.1.13-MariaDB : Database - superchat
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`superchat` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `superchat`;
+
+/*Table structure for table `message` */
+
+DROP TABLE IF EXISTS `message`;
+
+CREATE TABLE `message` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `message` text,
+  `received_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+/*Data for the table `message` */
+
+insert  into `message`(`id`,`username`,`message`,`received_date`) values (8,'armdev','Privet','2017-06-15 15:43:06'),(9,'armdev','4141','2017-06-15 15:43:19'),(10,'armdev','Mard ka?','2017-06-15 15:43:24'),(11,'armdev','Mard ka?','2017-06-15 15:43:28'),(12,'armdev','Privet','2017-06-15 15:43:39'),(13,'armdev','41471414','2017-06-15 15:43:43');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
