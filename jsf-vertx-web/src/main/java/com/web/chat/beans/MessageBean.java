cb334343bc5f9aa7e2f254e29d3c5f706d88c8a4
armena <armena@YLT160003.corp.optym.net>
2020-01-25 14:56:48 +0400

    update me

--- jsf-vertx-web/src/main/java/com/web/chat/beans/MessageBean.java
+++ jsf-vertx-web/src/main/java/com/web/chat/beans/MessageBean.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 import lombok.Getter;
